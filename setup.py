130fc15c676c2c46ed6e438399c4d3959a3709fb
Lukas Herman <lukashh6@gmail.com>
2017-02-19 19:50:18 -0500

    Fix setup script

--- setup.py
+++ setup.py
@@ -1,12 +1,4 @@
-from distutils.core import setup
+import pip
+import os
 
-with open('dependencies.txt') as file:
-    dependencies = file.read().strip().split('\n')
-
-setup(name='investment-visualizer',
-      version='1.0',
-      author='Lukas Herman',
-      author_email='lukashh6@gmail.com',
-      install_requires=dependencies,
-      packages=['investment_visualizer'],
-      )
+pip.main(['install', '-r', 'dependencies.txt'])
